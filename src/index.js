237c0f5795970585308db917cf225c363fc01626
Viktor <viktorpuke@gmail.com>
2020-05-04 14:08:56 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import Phaser from "phaser";
 
-import PlayGame from './scene';
+import PlayGame from "./scene";
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-      default: 'arcade',
-      arcade: {
-          gravity: { y: 300 },
-          debug: false
-      }
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
   },
-  scene: PlayGame
+  scene: PlayGame,
 };
 
 let game = new Phaser.Game(config);
