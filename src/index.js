37a01858a1fb9ca85944e4bd802f44df5e1775f2
Viktor <viktorpuke@gmail.com>
2020-05-08 14:11:11 +0200

    steering done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      // gravity: { y: 100 },
+      gravity: { y: 200 },
       debug: false,
     },
   },
--- src/scene.js
+++ src/scene.js
@@ -7,24 +7,27 @@ let cursors;
 
 class PlayGame extends Phaser.Scene {
   preload() {
-    this.load.image('space', require('./assets/space.png'));
-    this.load.spritesheet('ship', require('./assets/ship.png'), {frameWidth: 75, frameHeight: 160})
+    this.load.image("space", require("./assets/space.png"));
+    this.load.spritesheet("ship", require("./assets/ship.png"), {
+      frameWidth: 75,
+      frameHeight: 160,
+    });
   }
 
   create() {
     space = this.add.tileSprite(0, 0, 800, 600, "space").setOrigin(0, 0);
     backgroundVelocity = 1;
 
-    ship = this.physics.add.sprite(400, 500, 'ship').setScale(0.6);
+    ship = this.physics.add.sprite(400, 500, "ship").setScale(0.6);
     ship.setBounce(0.2);
     ship.setCollideWorldBounds(true);
 
     this.anims.create({
-      key: 'fly',
-      frames: this.anims.generateFrameNames('ship', {start: 0, end: 3}),
+      key: "fly",
+      frames: this.anims.generateFrameNames("ship", { start: 0, end: 3 }),
       frameRate: 10,
-      repeat: -1
-    })
+      repeat: -1,
+    });
 
     cursors = this.input.keyboard.createCursorKeys();
   }
@@ -32,30 +35,43 @@ class PlayGame extends Phaser.Scene {
   update() {
     space.tilePositionY += backgroundVelocity;
 
-    ship.anims.play('fly', true);
+    ship.anims.play("fly", true);
 
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown && cursors.up.isDown) {
       ship.setVelocityX(-160);
-    }
-    else if (cursors.right.isDown)
-    {
-      ship.setVelocityX(160);
-    }
-    else if (cursors.up.isDown)
-    {
       ship.setVelocityY(-160);
     }
-    else if (cursors.down.isDown)
-    {
-      ship.setVelocityY(160);
-    }
-    else
-    {
-      ship.setVelocityX(0);
+
+    if (cursors.right.isDown && cursors.up.isDown) {
+      ship.setVelocityX(160);
+      ship.setVelocityY(-160);
     }
 
-    //ship.body.position.y
+    if (cursors.right.isDown && cursors.down.isDown) {
+      ship.setVelocityX(160);
+      ship.setVelocityY(160);
+    }
+
+    if (cursors.left.isDown && cursors.down.isDown) {
+      ship.setVelocityX(-160);
+      ship.setVelocityY(160);
+    }
+
+    if (cursors.up.isDown) {
+      ship.setVelocityY(-160);
+    } else if (cursors.down.isDown) {
+      ship.setVelocityY(160);
+    } else {
+      ship.setVelocityY(0);
+    }
+
+    if (cursors.left.isDown) {
+      ship.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+      ship.setVelocityX(160);
+    } else {
+      ship.setVelocityX(0);
+    }
   }
 }
 
